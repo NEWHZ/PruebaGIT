064ad4ade01a797c583677f89d5e53428176bf5a
NEWHZ <nosoriom1@miumg.edu.gt>
2024-08-24 08:39:29 -0600

    Add files via upload

---
+++ GIT/scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+// Ejemplo de script personalizado
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Documento cargado y listo para ejecutarse.");
+    // Aquí puedes agregar tus scripts personalizados
+});
